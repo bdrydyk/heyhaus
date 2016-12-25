9056f042b087889baf510a01b49b753a56abb9b1
Doug Black <dblack@twilio.com>
2016-12-24 23:54:21 -0800

    added webpower

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from webpower import WebPowerSwitch
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/webpower/")
+@app.route("/webpower/<outlet>/")
+@app.route("/webpower/<outlet>/<state>")
+def webpower(outlet=None,state=None):
+	w = WebPowerSwitch()
+	states = w.get_states()
+
+	if outlet ==None and state==None:
+		return(str(states))
+
+	if outlet != None and state == None:
+		return(str(w.outlet[int(outlet)]))
+
+	if outlet != None and state != None:
+		if state == "ON":
+			state=True
+		elif state == "OFF":
+			state=False
+		return(str(w.set_outlet(str(outlet),state)))
+
 
 if __name__ == "__main__":
-    app.run()
+	app.run(host="0.0.0.0",debug=True)
--- webpower.py
+++ webpower.py
@@ -17,51 +17,42 @@ logger.addHandler(ch)
 logger.setLevel(logging.INFO)
 
 
-def byte_to_binary(n):
-	return ''.join(str((n & (1 << i)) and 1) for i in (range(8)))
-
-def hex_to_binary(h):
-	return ''.join(byte_to_binary(ord(b)) for b in binascii.unhexlify(h))
-
 class WebPowerSwitch(object):
 	"""docstring for WebPowerSwitch"""
 	def __init__(self):
 		super(WebPowerSwitch, self).__init__()
-		self.base_url = BASE_URL
+		self.base_url = "http://admin:seebeck10@10.0.1.67/"
+		self.outlet = [False,False,False,False,False,False,False,False,False]
+		self.get_states()
 
-		self.outlet1 = False
-		self.outlet2 = False
-		self.outlet3 = False
-		self.outlet4 = False
-		self.outlet5 = False
-		self.outlet6 = False
-		self.outlet7 = False
-		self.outlet8 = False
+	def byte_to_binary(self,n):
+		return ''.join(str((n & (1 << i)) and 1) for i in (range(8)))
 
-		self.getOutletStates()
+	def hex_to_binary(self,h):
+		return ''.join(self.byte_to_binary(ord(b)) for b in binascii.unhexlify(h))
 
-	def getOutletStates(self):
+	def get_states(self):
 		r = requests.get(self.base_url + 'index.htm')
 		m = re.search("<!-- state=(\S\S) lock=(\S\S) -->",r.text)
-		on_states = hex_to_binary(m.group(1))
-		lock_states = hex_to_binary(m.group(2))
+		on_states = self.hex_to_binary(m.group(1))
+		lock_states = self.hex_to_binary(m.group(2))
 
-		self.outlet1 = bool(int(on_states[0]))
-		self.outlet2 = bool(int(on_states[1]))
-		self.outlet3 = bool(int(on_states[2]))
-		self.outlet4 = bool(int(on_states[3]))
-		self.outlet5 = bool(int(on_states[4]))
-		self.outlet6 = bool(int(on_states[5]))
-		self.outlet7 = bool(int(on_states[6]))
-		self.outlet8 = bool(int(on_states[7]))
+		self.outlet[0] = on_states
+		self.outlet[1] = bool(int(on_states[0]))
+		self.outlet[2] = bool(int(on_states[1]))
+		self.outlet[3] = bool(int(on_states[2]))
+		self.outlet[4] = bool(int(on_states[3]))
+		self.outlet[5] = bool(int(on_states[4]))
+		self.outlet[6] = bool(int(on_states[5]))
+		self.outlet[7] = bool(int(on_states[6]))
+		self.outlet[8] = bool(int(on_states[7]))
 		return on_states
 
-
-	def outlet(self,outlet,state):
+	def set_outlet(self,outlet,state):
 		if state == True:
 			state="ON"
 		elif state == False:
 			state="OFF"
 		requests.get(self.base_url + "outlet", params={str(outlet):state})
-		return self.getOutletStates()
+		return self.get_states()
 
